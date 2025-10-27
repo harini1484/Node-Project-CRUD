d097039fa7c4c44a3bdac6fd1e884450b44d08bd
Harini <harini140804@gmail.com>
2025-10-27 11:27:33 +0530

    Added validation for register and login with .com check

--- Controller/Usercontroller.js
+++ Controller/Usercontroller.js
@@ -1,56 +1,59 @@
 const usermodel = require("../Model/Usermodel");
+const { adduserschema } = require("../Validation/Uservalidation");
 
 const adduser = async (req, res) => {
-    try {
-        let user = new usermodel({
-            name: req.body.name,
-            email: req.body.email,
-            phone: req.body.phone
-        });
+  try {
+    const { error } = adduserschema.validate(req.body);
+    if (error) return res.json({ message: error.message });
 
-        await user.save();
-        res.json({ message: "user created successfully" });
-    } catch (error) {
-        console.error(error);
-        res.json({ message: "error occurred" });
-    }
+    let user = new usermodel({
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone
+    });
+
+    await user.save();
+    res.json({ message: "user created successfully" });
+  } catch (error) {
+    console.error(error);
+    res.json({ message: "error occurred" });
+  }
 };
 
-
 const getuser = async (req, res) => {
-    try {
-        const users = await usermodel.find();
-        res.json(users);
-    } catch (error) {
-        res.json({ error });
-    }
+  try {
+    const users = await usermodel.find();
+    res.json(users);
+  } catch (error) {
+    res.json({ error });
+  }
 };
 
 const updateuser = async (req, res) => {
-    try {
-        const updated = await usermodel.findByIdAndUpdate(req.params.id, req.body);
-      res.json({ message: "user updated successfully" })
-    } catch (error) {
-        res.json({ error });
-    }
+  try {
+    const updated = await usermodel.findByIdAndUpdate(req.params.id, req.body);
+    res.json({ message: "user updated successfully" });
+  } catch (error) {
+    res.json({ error });
+  }
 };
 
 const deleteuser = async (req, res) => {
-    try {
-        const updated = await usermodel.findByIdAndDelete(req.params.id);
-    res.json({ message: "user deleted successfully" })
-    } catch (error) {
-        res.json({ error });
-    }
+  try {
+    const deleted = await usermodel.findByIdAndDelete(req.params.id);
+    res.json({ message: "user deleted successfully" });
+  } catch (error) {
+    res.json({ error });
+  }
 };
 
 const getbyid = async (req, res) => {
-    try {
-        const getuser = await usermodel.findByIdAndDelete(req.params.id);
-    res.json({ getuser})
-    } catch (error) {
-        res.json({ error });
-    }
+  try {
+    const getuser = await usermodel.findById(req.params.id);
+    res.json({ getuser });
+  } catch (error) {
+    res.json({ error });
+  }
 };
 
 module.exports = { adduser, getuser, updateuser, deleteuser, getbyid };
